ebfbcb90799d4a4f3c8fd1da0eb0f508bf87d9f5
Lucas Rodrigues <Lucasrodrigues4@live.com>
2025-03-25 19:54:18 -0300

    formatação apenas

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,31 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('botao-cadastro').addEventListener('click', function(event) {
-        event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("botao-cadastro")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
 
-        const nome = document.getElementById('nome').value;
-        const email = document.getElementById('email').value;
-        const senha = document.getElementById('senha').value;
-        const erro = document.getElementById("erro");
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+      const senha = document.getElementById("senha").value;
+      const erro = document.getElementById("erro");
 
-        if (!nome || !email || !senha) {
-            alert('Por favor, preencha todos os campos!');
-            return;
-        }
-        
-        let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+      if (!nome || !email || !senha) {
+        alert("Por favor, preencha todos os campos!");
+        return;
+      }
 
-        const emailExistente = usuarios.some(usuario => usuario.email === email);
-        if (emailExistente) {
-            alert('Este e-mail já está cadastrado!');
-            return;
-        }
+      let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
 
-        const novoUsuario = { nome, email, senha };
-        usuarios.push(novoUsuario);
+      const emailExistente = usuarios.some(
+        (usuario) => usuario.email === email
+      );
+      if (emailExistente) {
+        alert("Este e-mail já está cadastrado!");
+        return;
+      }
 
-        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+      const novoUsuario = { nome, email, senha };
+      usuarios.push(novoUsuario);
 
-        alert('Usuário cadastrado com sucesso!');
-        window.location.href = '/sistema/pages/login.html';
+      localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+      alert("Usuário cadastrado com sucesso!");
+      window.location.href = "/sistema/pages/login.html";
     });
 });
